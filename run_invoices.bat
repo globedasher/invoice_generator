29b9898641d08a7b564934b7f8797ae435566b42
globedasher <github@cow.simplelogin.com>
2025-09-29 18:33:52 -0700

    Update batch

--- run_invoices.bat
+++ run_invoices.bat
@@ -1,10 +1,17 @@
 @echo off
+echo Activating virtual environment...
+call venv\Scripts\activate.bat
+
 echo Installing/updating dependencies...
 pip install -r requirements.txt
 
 echo.
 echo Generating invoices...
 python generate_invoices.py
+
+echo.
+echo Deactivating virtual environment...
+deactivate
 
 echo.
 echo Press any key to exit...
